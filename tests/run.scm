873d56223e957ebd3649cd1ce10b9921b910538f
Peter Danenberg <pcd@roxygen.org>
2013-09-13 07:14:35 -0700

    Retest the version comparison.

--- tests/run.scm
+++ tests/run.scm
@@ -1,6 +1,6 @@
 (use cock-utils
      test)
 
-(test-assert (version<? "1.2.3" "1.2.4"))
-(test-assert (version<? "1.2" "1.2.4"))
-(test-assert (version<? "1.2.3" "1.2.10"))
+(test-assert (version<=? "1.2.3" "1.2.4"))
+(test-assert (version<=? "1.2" "1.2.4"))
+(test-assert (version<=? "1.2.3" "1.2.10"))
