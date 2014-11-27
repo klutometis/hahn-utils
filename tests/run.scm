1586d8ec5385112ddeccbae738f7dc3dc811147f
Peter Danenberg <danenberg@google.com>
2014-11-27 13:44:21 -0800

    0.4.3

--- tests/run.scm
+++ tests/run.scm
@@ -1,6 +1,7 @@
 (use cock-utils
      test)
 
+(test-assert (version<=? "0.0.2" "0.1.1"))
 (test-assert (version<=? "1.2.3" "1.2.4"))
 (test-assert (version<=? "1.2" "1.2.4"))
 (test-assert (version<=? "1.2.3" "1.2.10"))
