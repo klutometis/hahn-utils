ba6a5848320246787f7cf19768d0f8191d3e0849
Peter Danenberg <danenberg@google.com>
2014-11-28 13:30:52 -0800

    Test the converse, too.

--- tests/run.scm
+++ tests/run.scm
@@ -1,7 +1,13 @@
 (use cock-utils
      test)
 
+(test-assert (version<=? "0.1" "0.3.4"))
+(test-assert (not (version<=? "0.3.4" "0.1")))
 (test-assert (version<=? "0.0.2" "0.1.1"))
+(test-assert (not (version<=?  "0.1.1" "0.0.2")))
 (test-assert (version<=? "1.2.3" "1.2.4"))
+(test-assert (not (version<=? "1.2.4" "1.2.3")))
 (test-assert (version<=? "1.2" "1.2.4"))
+(test-assert (not (version<=? "1.2.4" "1.2")))
 (test-assert (version<=? "1.2.3" "1.2.10"))
+(test-assert (not (version<=? "1.2.10" "1.2.3")))
