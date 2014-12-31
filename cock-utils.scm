f5531ca9c0afa900d705e59a8c825bdc57e7063a
Peter Danenberg <danenberg@google.com>
2014-12-31 06:30:29 -0800

    Document cock-utils.

--- cock-utils.scm
+++ cock-utils.scm
@@ -1,8 +1,35 @@
-@(title "cock-utils")
-@(description "Utilities for translating in-source docs to e.g. wiki, latex")
-@(author "Peter Danenberg")
-@(username "klutometis")
-@(email "pcd@roxygen.org")
+@(heading "Introduction")
+
+@(text "{{Cock-utils}} is mainly interesting because it provides the {{cock}}
+program that takes code documented with [[cock]] and converts
+it into documentation.")
+
+@(text "{{Cock-utils}} is a soft-dependency and shouldn't be included
+in {{depends}}.")
+
+@(heading "Invocation")
+
+@(text "{{Cock-utils}} is normally invoked from a {{.setup}} file; 
+see [[https://wiki.call-cc.org/eggref/4/cock#the-setupfile|this
+example]]:")
+
+@(source
+  (use cock setup-helper-mod)
+
+  (setup-shared-extension-module
+   'landauer
+   (extension-version "0.0.1")
+   compile-options: '(-X cock))
+
+  (run-cock -o landauer.wiki landauer.scm landauer-core.scm))
+
+@(text "It can also be run from the command line:
+  cock -o landauer.wiki landauer.scm landauer-core.scm")
+
+@(text "See {{cock --help}} for details.")
+
+@(heading "Documentation")
+@(noop)
 
 (module cock-utils
   @("The cock-parse module is responsible for the heavy lifting:
